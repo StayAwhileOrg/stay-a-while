a15c4f9d02c1b942497e81a3f6e5e810d8ce2986
julieoyen <juloye51070@stud.noroff.no>
2025-03-26 13:45:18 +0100

    Added typescript to home and fetchCabins

--- src/hooks/api/ui/fetchCabins.tsx
+++ src/hooks/api/ui/fetchCabins.tsx
@@ -1,14 +1,37 @@
-import {API_BASE} from "../../../utility/constants.tsx";
+import { API_BASE } from '../../../utility/constants.tsx';
 
-export async function getCabins(params){
-    try {
-        const searchParams = new URLSearchParams(params).toString();
-        const res:Response = await fetch(`${API_BASE}/cabin?${searchParams}`);
-        const data = await res.json();
-        console.log(data)
-        return data.data;
-    }catch (error) {
-        console.error("There was an error fetching cabins", error)
-        throw error;
-    }
+type Cabin = {
+  _id: string;
+  title: string;
+  pricePerNight: number;
+  location: {
+    city: string;
+    country: string;
+  };
+  images: {
+    imgURL: string;
+    imgAlt?: string;
+  }[];
+  facilities: {
+    beds: number;
+    smokingAllowed: boolean;
+    petsAllowed: boolean;
+    wifi: boolean;
+    electricity: boolean;
+  };
+};
+
+export async function getCabins(
+  params?: Record<string, string>
+): Promise<Cabin[]> {
+  try {
+    const searchParams = new URLSearchParams(params).toString();
+    const res: Response = await fetch(`${API_BASE}/cabin?${searchParams}`);
+    const data = await res.json();
+    console.log(data);
+    return data.data;
+  } catch (error) {
+    console.error('There was an error fetching cabins', error);
+    throw error;
+  }
 }
